0a40318129869a8a1f16a72aa7a326c79e96cf98
MislankaNova <mislankanova@hotmail.com>
2016-12-20 21:35:19 +0000

    traits.rs now used typedefs.

--- src/fourthrail/traits.rs
+++ src/fourthrail/traits.rs
@@ -1,16 +1,18 @@
 // Traits used by subte
 
+use fourthrail::typedefs::*;
+
 pub trait Agent {
     // Force is yet to be defined.
     //fn act(&self) -> Force;
 }
 
 pub trait Display {
-    fn display(&self) -> (i16, char);
+    fn display(&self) -> Icon;
 }
 
 pub trait Position {
-    fn pos(&self) -> (i32, i32);
+    fn pos(&self) -> Coord;
 }
 
 pub trait Named {
