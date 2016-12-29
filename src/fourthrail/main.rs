b2f6cc33b4028e9cc0b3659d71b0c0b8e288a0b7
MislankaNova <mislankanova@hotmail.com>
2016-12-29 18:25:58 +0000

    Modulised Fourthrail.update_graphic.
    Squashed commit of the following:
    
    commit 72ffeb5d00bf97ec5a72d29dd206bdcdf84d9d39
    Author: MislankaNova <mislankanova@hotmail.com>
    Date:   Thu Dec 29 18:25:27 2016 +0000
    
        Fourthrail.update_graphic is now modulised.
    
    commit b33754a5091d0855fe4e5ab45a69e1da12e37f2c
    Author: MislankaNova <mislankanova@hotmail.com>
    Date:   Thu Dec 29 18:22:01 2016 +0000
    
        Added fn show_map and fn show_status.

--- src/fourthrail/main.rs
+++ src/fourthrail/main.rs
@@ -61,7 +61,7 @@ impl<'trip> Fourthrail<'trip> {
                     self.update_visibility();
                 }
             },
-            
+
             GameState::Message => ()
         }
     }
@@ -104,6 +104,23 @@ impl<'trip> Fourthrail<'trip> {
     }
 
     pub fn update_graphic(&self) {
+        // This is a hack
+        // Without this the map cannot update properly
+        self.window.border(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+
+        match self.state {
+            GameState::Map => {
+                self.show_map();
+                self.show_status();
+            },
+
+            GameState::Message => ()
+        }
+
+        self.window.refresh();
+    }
+
+    fn show_map(&self) {
         let (mut pr, mut pc) = self.player.get_coord();
         pr -= (graphic::MAP_DISPLAY_HEIGHT - graphic::MAP_DISPLAY_STEP) / 2;
         pc -= (graphic::MAP_DISPLAY_WIDTH - graphic::MAP_DISPLAY_STEP) / 2;
@@ -113,12 +130,11 @@ impl<'trip> Fourthrail<'trip> {
         pc = graphic::MAP_DISPLAY_STEP * (pc / graphic::MAP_DISPLAY_STEP);
         let start = (pr, pc);
 
-        // This is a hack
-        // Without this the map cannot update properly
-        self.window.border(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
         graphic::put_map(&self.window, start, &self.map, &self.map_memory);
-        graphic::put_stats(&self.window, &self.coherency);
         graphic::put_creature(&self.window, start, &self.player);
-        self.window.refresh();
+    }
+
+    fn show_status(&self) {
+        graphic::put_stats(&self.window, &self.coherency);
     }
 }
